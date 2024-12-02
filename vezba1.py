8c601f4ad999b8c06c3f042c132298825b8290cf
jovana94f <jvasic.vasic@gmail.com>
2024-12-02 20:58:16 +0100

    dodat fajl vezba2.py

---
+++ vezba1.py
@@ -0,0 +1,10 @@
+print("Ubacite nesto u korpu")
+naziv_proizvoda = input("Unesite naziv:")
+cena =input("Unesite cenu:")
+print("Nove izmene na developu... dev grana")
+
+# print("Dodajem nove izmene")
+# print("Novi red")
+
+print("Uspesno ste uneli:", naziv_proizvoda, cena)
+ 
